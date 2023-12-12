447e7c41570a46eeca89b0555af69c7da46e5d0c
Erwan Gouriou <erwan.gouriou@st.com>
2023-12-12 14:04:28 +0100

    sample: code_relocation: test on SRAM3 on STM32H5
    Requires a small fix on sample sample linker script for ext flash simulation
    
    Compilation issue with H5 script should be investigated.
    
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- samples/application_development/code_relocation_nocopy/src/sram_code.c
+++ samples/application_development/code_relocation_nocopy/src/sram_code.c
@@ -9,8 +9,15 @@
 
 uint32_t var_sram_data = 10U;
 
+uint8_t firmware_image[10] = {0,1,2,3,4,5,6,7,8,9};
+
 void function_in_sram(void)
 {
 	printk("Address of %s %p\n", __func__, &function_in_sram);
 	printk("Address of var_sram_data %p (%d)\n", &var_sram_data, var_sram_data);
+	printk("Address of firmware_image %p \n", &firmware_image);
+
+	for (uint32_t i = 0U; i <10; i++) {
+		printk("firmware_image[%d]:%d\n", i, firmware_image[i]);
+	}
 }
